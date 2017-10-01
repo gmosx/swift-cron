e5eb282f1a89d15eb50a1aec38e00a0951729eb9
George Moschovitis <gmosx@reizu.com>
2017-10-01 20:48:00 +0300

    Schedule cron jobs on the dispatchQueue

--- Sources/Cron/Cron.swift
+++ Sources/Cron/Cron.swift
@@ -13,7 +13,7 @@ struct CronItem {
 
 struct FunctionJob: Job {
     let fn: () -> Void
-
+    
     func run() {
         fn()
     }
@@ -23,55 +23,58 @@ public class Cron {
     var items: [CronItem]
     let dispatchQueue: DispatchQueue
     var _isRunning: Bool
-
+    
     public init() {
         items = []
         dispatchQueue = DispatchQueue(label: "cronQueue", attributes: .concurrent)
         _isRunning = false
     }
-
+    
     public func schedule(pattern: DatePattern, job: Job) {
         items.append(CronItem(pattern: pattern, job: job))
     }
-
+    
     public func schedule(pattern: String, job: Job) {
         schedule(pattern: DatePattern(pattern)!, job: job)
     }
-
+    
     public func schedule(pattern: DatePattern, fn: @escaping () -> Void) {
         items.append(CronItem(pattern: pattern, job: FunctionJob(fn: fn)))
     }
-
+    
     public func schedule(pattern: String, fn: @escaping () -> Void) {
         schedule(pattern: DatePattern(pattern)!, fn: fn)
     }
-
+    
     public func start() {
         _isRunning = true
         run()
     }
-
+    
     public func stop() {
         _isRunning = false
     }
-
+    
     public var isRunning: Bool {
         return _isRunning
     }
-
+    
     public func run() {
         while _isRunning {
-            let nowDate = Date()
-
-            Log.verbose("Cron tick")
-
-            for item in items {
-                if item.pattern.isMatching(nowDate) {
-                    item.job.run()
+            dispatchQueue.async {
+                Log.verbose("Cron tick")
+                
+                let nowDate = Date()
+                
+                for item in self.items {
+                    if item.pattern.isMatching(nowDate) {
+                        item.job.run()
+                    }
                 }
             }
-
+            
             Thread.sleep(forTimeInterval: tickInterval)
         }
     }
 }
+
